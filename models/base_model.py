0b357418cfa9a851d40fc03e8536acb7d1e6ef73
Esther Ndosi <estherndosi24@gmail.com>
2024-05-27 07:22:00 -0700

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Define a Base class"""
+from datetime import datetime
+import uuid
+
+
+class BaseClass:
+    """Base model.
+    This class represent the base of all classes in AirBnB project
+    """
+
+    def __init__(self):
+        """Initializes a new Base instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Prints the string representation of an object"""
+        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """Updates the update_at attribute"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """Returns dictionary containing all the keys/values"""
+        dict_b = self.__dict__
+        dict_b['__class__'] = self.__class__.__name__
+        dict_b['created_at'] = datetime.isoformat(self.created_at)
+        dict_b['updated_at'] = datetime.isoformat(self.updated_at)
+
+        return dict_b

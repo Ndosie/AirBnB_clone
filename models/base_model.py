373ef30af6a5b70bbfdd4c7a68ab1cf6c084e6ed
Esther Ndosi <estherndosi24@gmail.com>
2024-05-30 03:38:50 -0700

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
 
     def __str__(self):
         """Prints the string representation of an object"""
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the update_at attribute"""

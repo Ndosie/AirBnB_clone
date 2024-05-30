8af172025b3c10b14fc51ad4f8e2575aae9897d0
Esther Ndosi <estherndosi24@gmail.com>
2024-05-30 04:36:52 -0700

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,18 @@ class BaseModel:
     This class represent the base of all classes in AirBnB project
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new Base instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs and len(kwargs) != 0:
+            for k,v in kwargs.items():
+                if k != '__class__':
+                    if k == 'created_at' or k == 'updated_at':
+                        self.__dict__[k] = datetime.strptime(v, "%Y-%m-%d %H:%M%S.%f") 
+                    else:
+                        self.__dict__[k] = v
 
     def __str__(self):
         """Prints the string representation of an object"""
